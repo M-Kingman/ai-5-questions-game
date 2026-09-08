1a47ddadeaa788ac49071a73e1fa0245180833ac
Masaru Kingman <masaru.kingman+git@gmail.com>
2026-09-08 13:42:51 +0200

    Implement neuron forward pass

--- learning/v0.1_single_neuron/single_neuron.py
+++ learning/v0.1_single_neuron/single_neuron.py
@@ -1,3 +1,5 @@
+import math
+
 question_1 = "Do you struggle to see texts in books/newspapers?"
 question_2 = "Do you struggle to see objects far away?"
 question_3 = "Do you struggle to see an object clearly that is 6 meters away?"
@@ -6,24 +8,27 @@ questions = [question_1, question_2, question_3]
 inputs = [0, 0, 0]
 weights = [0.7, 0.4, 0.7]
 bias = 0
-final_answer = ""
 
 
 def neural_equation(inputs, weights, bias):
     """Weighted-sum equation (1 neuron)"""
     answer = ""
     equation = (inputs[0] * weights[0]) + (inputs[1] * weights[1]) + (inputs[2] * weights[2]) + bias
+    # Sigmoid Function
+    output = 1 / (1 + math.e ** -equation)
+    return output
 
-    # Statement below is currently just used for testing
-    if equation <= 0.5:
-        answer = "Yes, you should probably go for an eye test."
-    else:
+
+def final_answer(output):
+    """Takes output from neural_equation() and provides final answer"""
+    if output <= 0.5:
         answer = "No,you probably dont need an eye test."
+    else:
+        answer = "Yes, you should probably go for an eye test."
     return answer
 
 
 def main():
-
     counter = 0
 
     # Asks questions and takes users inputs
@@ -41,7 +46,7 @@ def main():
 
         counter += 1
 
-    print(neural_equation(inputs, weights, bias))
+    print(final_answer(neural_equation(inputs, weights, bias)))
 
 
 if __name__ == "__main__":
