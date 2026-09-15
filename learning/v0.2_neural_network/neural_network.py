Fix training data coordinate normalization
Training data was previously using board coordinates between 0 and 7
while player inputs were being normalized between 0 and 1.

This caused the neural network to receive different input types during training and gameplay.
Therefore, giving wrong predictions, which got worse with more training.

Updated the training data format to store chess positions such as c2 and b55.
read_training_data() now converts training data start/end positions using position_conversion().

The training data now produces the same numerical representation used by player input.

learning/v0.2_neural_network/neural_network.py
@@ -11,7 +11,7 @@ class NeuralNetwork:
         self.output_weights = rng.uniform(low=-0.5, high=0.5, size=21)
         self.output_bias = 0.5
         self.learning_rate = 0.1
-        self.epochs = 1000
+        self.epochs = 5000
 
     def sigmoid(self, weighted_sum):
         return 1 / (1 + np.exp(-weighted_sum))
@@ -118,8 +118,10 @@ class NeuralNetwork:
                 self.update_parameters(backpropagation_data)
 
             # Test for during development, to check if loss decreases
-            avg_loss = total_loss / len(training_data)
-            print(f"Epoch {epoch + 1}: loss = {avg_loss} ")
+
+            if (epoch + 1) % 100 == 0:
+                avg_loss = total_loss / len(training_data)
+                print(f"Epoch {epoch + 1}: loss = {avg_loss} ")
 
     def update_model(self, input_hidden_weights, hidden_biases, output_weights, output_bias):
         "Uses data from load_model() to update the model weights and biases"