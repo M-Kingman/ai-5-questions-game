659b180fed74d88deaa460c287692f469f6238c9
Masaru Kingman <masaru.kingman+git@gmail.com>
2026-09-15 17:25:54 +0200

    Add YOLO11 object detection

---
+++ 5Q_game/src/vision.py
@@ -0,0 +1,22 @@
+from ultralytics import YOLO
+
+
+class Vision:
+    def __init__(self):
+        self.model = YOLO("yolo11n.pt")
+
+    def detect_objects(self, image_path):
+        """Returns a unique list of objects that are in the scene"""
+        results = self.model(image_path)[0]
+        objects = []
+        for object in results.boxes:
+            class_id = int(object.cls[0])
+            object_name = self.model.names[class_id]
+            objects.append(object_name)
+
+        # Gets rid of duplicates
+        objects = list(set(objects))
+        return objects
+
+vision = Vision()
+print(vision.detect_objects("../scenes/kitchen.jpg"))
