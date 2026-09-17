f0b491f91f537afb7193b698b63e0383d6eaf26b
Masaru Kingman <masaru.kingman+git@gmail.com>
2026-09-17 16:17:39 +0200

    Add GameState structure

--- 5Q_game/src/game_state.py
+++ 5Q_game/src/game_state.py
@@ -6,3 +6,29 @@ class GameState:
     """Keeps track of the overall game state"""
     # Only creates one object representing the game
 
+    def __init__(self, detected_objects):
+
+        # Objects detected by vision
+        self.detected_objects = detected_objects
+
+        # Keeps track of which questions have been asked
+        self.question_tracker = QUESTION_LIST.copy()
+
+        # Uses question tracker to convert question states into numerical inputs
+        self.answer_input_list = [[0, 0, 0, 0] for _ in range(14)]
+
+        # Tracks how many questions have been asked
+        self.questions_asked = 0
+
+        # Assigns a value between 0 and 1 (for input)
+        self.questions_remaining = (5 - self.questions_asked) / 5
+
+        self.game_over = False
+
+        # Stores all 16 objects from ObjectState
+        self.object_states = []
+
+
+
+
+
