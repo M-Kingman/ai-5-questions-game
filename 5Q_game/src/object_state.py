9b7ff715344e7355ad4b3b57037408f31f29f20f
Masaru Kingman <masaru.kingman+git@gmail.com>
2026-09-17 15:50:57 +0200

    Add ObjectState structure

--- 5Q_game/src/object_state.py
+++ 5Q_game/src/object_state.py
@@ -2,3 +2,21 @@ class ObjectState:
     """Keeps track of each objects state"""
     # Creates an object for each object in OBJECTS
 
+    def __init__(self, name, properties, property_input_values, indicator, probability):
+
+        self.name = name
+
+        # Stores original property values
+        self.properties = properties
+
+        # Set once at the beginning of the game
+        # values depend on objects presence
+        self.property_input_values = property_input_values
+        self.indicator = indicator
+
+        # Changes throughout game
+        self.probability = probability
+
+
+
+
