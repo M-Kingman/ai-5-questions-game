f7ad6af8729641b2c46a5a971910ea4335dd3254
Masaru Kingman <masaru.kingman+git@gmail.com>
2026-09-17 15:17:57 +0200

    Add GameState & ObjectState classes

---
+++ 5Q_game/src/game_state.py
@@ -0,0 +1,8 @@
+from questions import *
+from objects import OBJECTS
+
+
+class GameState:
+    """Keeps track of the overall game state"""
+    # Only creates one object representing the game
+
---
+++ 5Q_game/src/object_state.py
@@ -0,0 +1,4 @@
+class ObjectState:
+    """Keeps track of each objects state"""
+    # Creates an object for each object in OBJECTS
+
